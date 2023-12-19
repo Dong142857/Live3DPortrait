30890bc31ff6c1d7235d2afdd10bfc4143ed0ffc
Dong142857 <47628302+Dong142857@users.noreply.github.com>
2023-12-19 21:07:58 +0800

    modified code (#1)
    modified inference code

---
+++ configs/paths_config.py
@@ -0,0 +1,11 @@
+dataset_paths = {
+}
+
+model_paths = {
+	'encoder_render': 'pretrained_models/encoder_render.pt',
+	'eg3d_ffhq': 'pretrained_models/ffhq512-128.pth',
+	'ir_se50': 'pretrained_models/model_ir_se50.pth',
+	'eg3d_rebalanced': 'pretrained_models/ffhqrebalanced512-128.pth',
+
+}
+
